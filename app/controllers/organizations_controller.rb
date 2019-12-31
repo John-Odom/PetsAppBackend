765636e398c10acac9fe617765d346b4c34f8c1c
John Odom <john.osborne.odom@gmail.com>
2019-12-31 12:33:51 -0500

    added show method to Org Controllerr

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,5 +3,9 @@ class OrganizationsController < ApplicationController
         orgs = Organization.all
         render json: orgs
     end
+    def show
+        org = Organization.find(params[:id])
+        render json: org
+    end
 
 end
