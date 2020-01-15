768977e5a735131ea18d2524a5937a258ea4ed5e
John Odom <john.osborne.odom@gmail.com>
2020-01-15 11:04:48 -0500

    updated dogs controller to create without Photos param

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -8,14 +8,11 @@ class DogsController < ApplicationController
         dog=Dog.find(params[:id])
         render json: dog
     end
-
-
         # since dog.photos is json, and not a string or integer, we must first declare
         # it as it's class, then we can fill in the array
         #Json causing problems with the contact[address] as well
         #JSON.parse would not work - JSON.parse(dog.contact['address'])
     def create
-        binding.pry
          dog=Dog.find_or_create_by!(
             name: params['dog']["name"], gender: params['dog']["gender"],
             size: params['dog']["size"], age: params['dog']["age"],
